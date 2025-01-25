d1f5e8be80c88f53d24b63a16371f1a999e7ac39
dose06 <128408906+dose06@users.noreply.github.com>
2025-01-25 21:06:00 +0900

    Add files via upload

--- Transformer/modules/__init__.py
+++
@@ -1,7 +0,0 @@
-# Import key components of the Transformer model
-from .encoder_decoder import EncoderDecoder, Generator
-from .encoder import Encoder, EncoderLayer, Decoder, DecoderLayer, SublayerConnection, LayerNorm
-from .attention import MultiHeadedAttention, attention
-from .positional_encoding import PositionwiseFeedForward, PositionalEncoding, Embeddings
-from .util.utils import subsequent_mask, clones
-from .batch import SimpleLossCompute, data_gen, loss, LabelSmoothing, NoamOpt, run_epoch, Batch
--- Transformer/transformer.py
+++ Transformer/transformer.py
@@ -552,13 +552,13 @@ class SimpleLossCompute:
 
 
 # Train the simple copy task.
-V = 5
+V = 11
 criterion = LabelSmoothing(size=V, padding_idx=0, smoothing=0.0)
 model = make_model(V, V, N=2)
 model_opt = NoamOpt(model.src_embed[0].d_model, 1, 400,
         torch.optim.Adam(model.parameters(), lr=0, betas=(0.9, 0.98), eps=1e-9))
 
-for epoch in range(5):
+for epoch in range(10):
     model.train()
     run_epoch(data_gen(V, 30, 20), model, SimpleLossCompute(model.generator, criterion, model_opt))
     model.eval()
